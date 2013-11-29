c50f51c37821c224bf49860d47509bfe1ade0137
Macacity <Macacity@gmail.com>
2013-11-29 11:38:48 +0100

    initial Commit

---
+++ fbin/PowerPoint.py
@@ -0,0 +1,51 @@
+__author__ = 'LPC'
+import comtypes.client as com
+import pythoncom
+import os
+
+class PowerPoint:
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.application, self.presentation = self.create()
+
+    def create(self):
+
+        pythoncom.CoInitialize()
+        application = com.CreateObject("PowerPoint.Application")
+        return application, application.Presentations.Open(os.path.abspath(self.filename))
+
+    def get_images_from_ppt(self, exportdir):
+        self.presentation.SaveAs(os.path.abspath(exportdir), 17)  # 17 = jpg
+
+
+    def quit(self):
+        self.presentation.Close()
+        self.application.Quit()
+        pythoncom.CoUninitialize()
+
+    def images_in_ppt(self):
+        if not self.presentation:
+            self.create()
+        for slide in self.presentation:
+            if self.image_in_slide(slide):
+                return True
+        return False
+
+    @staticmethod
+    def image_in_slide(slide):
+        for shape in slide.Shapes:
+                if shape.HasTextFrame == 0:
+                    return True
+        return False
+
+    def slides_with_images(self):
+        imageslides = []
+        for slide in self.presentation.Slides:
+            if self.image_in_slide(slide):
+                imageslides.append(slide.SlideNumber)
+        return imageslides
+
+    def slides(self):
+        return self.presentation.Slides
+        #TODO: Generator?
